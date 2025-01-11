efad046537154d4e7d7f52f1b4c05aded33958dc
Kaushik Patil <kaushikpatil530@gmail.com>
2025-01-11 14:46:50 +0530

    Add files via upload

---
+++ DisplayMarksInSortedWay.py
@@ -0,0 +1,7 @@
+a = int(input("Enter PHYSICS MARKS HERE: \t"))
+b = int(input("Enter Mathmatics MARKS HERE: \t "))
+c = int(input("Enter BIOLOGY MARKS HERE: \t "))
+
+myList = [ a , b , c ]
+myList.sort()
+print(myList)
